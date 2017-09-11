c2c94b46b7b093a442d6793c4451f09b5c83c830
hauenst <hauenst@jlab.org>
2017-09-11 00:02:30 -0400

    Updated perbinanalysis script for estimation of pion events from cross section

--- analysis/florian/perbinanalysis.cpp
+++ analysis/florian/perbinanalysis.cpp
@@ -52,7 +52,42 @@ const double DEG = 180./3.1415926;
 const double PI = 3.1415926;
 
 int main(Int_t argc, char *argv[]){
-  	int fA = 0; cerr<<"-- What nucleus: A = ? "; cin >> fA;
+
+    int fA = 0; cerr<<"-- What nucleus: A = ? "; cin >> fA;
+    int number_of_files = 1; cerr<<"How many files with 1M events? " ; cin >> number_of_files;
+    if (number_of_files !=50 && number_of_files !=500 && number_of_files!=1 && number_of_files!=5) {
+      cout << "This skim is not available. Number of input file is set to 1" << endl;
+      number_of_files = 1;
+    }
+
+
+    const Double_t EIC_Mom_Min_e = 8.5;
+    const Double_t EIC_Mom_Max_e = 10.5;
+    const Double_t EIC_Mom_Min_h = 0.0;
+    const Double_t EIC_Mom_Max_h = 10.0;
+
+    const Double_t EIC_Th_Min_e = 0.0;
+    const Double_t EIC_Th_Max_e  = 25.0;
+    const Double_t EIC_Th_Min_h = 0.0;
+    const Double_t EIC_Th_Max_h  = 180.0;
+
+    const Double_t EIC_Ph_Min_e = 0.0;
+    const Double_t EIC_Ph_Max_e  = 360.0;
+    const Double_t EIC_Ph_Min_h = 0.0;
+    const Double_t EIC_Ph_Max_h  = 360.0;
+
+    const Double_t degtorad = TMath::Pi()/180.;
+
+    Double_t generated_electron_phase_space =(cos(EIC_Th_Min_e*degtorad) - cos(EIC_Th_Max_e*degtorad))*(EIC_Ph_Max_e*degtorad - EIC_Ph_Min_e*degtorad)*(EIC_Mom_Max_e - EIC_Mom_Min_e);
+    Double_t generated_hadron_phase_space   =(cos(EIC_Th_Min_h*degtorad) - cos(EIC_Th_Max_h*degtorad))*(EIC_Ph_Max_h*degtorad - EIC_Ph_Min_h*degtorad)*(EIC_Mom_Max_h - EIC_Mom_Min_h);
+    Double_t generated_phase_space=generated_electron_phase_space*generated_hadron_phase_space;
+    cout << "generated PS: " << generated_phase_space << endl;
+
+    const double luminosity = 1e33; // unit 1/(cm^2 s)
+    const double nbarntocm2 = 1e-33;
+
+    double integrated_lumi1 = 1e6; // 1/fb = 10^6/nb
+    double integrated_lumi2 = 1e5; // 1/10fb = 10^5/nb
 
     double z_binmin = 0.2, z_binmax = 0.8;
     const int zbin = 3;
@@ -69,57 +104,131 @@ int main(Int_t argc, char *argv[]){
     histo_counts->GetYaxis()->CenterTitle(1);
     histo_counts->SetMarkerSize(2);
     histotitle.Clear();
-    if (fA == 2) histotitle = "Weighted total count rate d(e,e'#pi^{+})X; z ; Q2 (GeV^{2})";
-    if (fA == 12) histotitle = "Weighted total count rate ^{12}C(e,e'#pi^{+})X; z ; Q2 (GeV^{2})";
 
+    if (fA == 2) histotitle = "Weighted total cross section d(e,e'#pi^{+})X (MC error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Weighted total cross section ^{12}C(e,e'#pi^{+})X (MC error); z ; Q2 (GeV^{2})";
     TH2D *pionp_cross = new TH2D("pionp_cross ",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
     pionp_cross->GetXaxis()->CenterTitle(1);
     pionp_cross->GetYaxis()->CenterTitle(1);
     pionp_cross->SetMarkerSize(2);
     pionp_cross->Sumw2();
     histotitle.Clear();
-    if (fA == 2) histotitle = "Weighted total count rate d(e,e'#pi^{-})X; z ; Q2 (GeV^{2})";
-    if (fA == 12) histotitle = "Weighted total count rate ^{12}C(e,e'#pi^{-})X; z ; Q2 (GeV^{2})";
 
+    if (fA == 2) histotitle = "Weighted total cross section d(e,e'#pi^{-})X (MC error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Weighted total cross section ^{12}C(e,e'#pi^{-})X (MC error); z ; Q2 (GeV^{2})";
     TH2D *pionm_cross = new TH2D("pionm_cross",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
     pionm_cross->GetXaxis()->CenterTitle(1);
     pionm_cross->GetYaxis()->CenterTitle(1);
     pionm_cross->SetMarkerSize(2);
     pionm_cross->Sumw2();
     histotitle.Clear();
-    if (fA == 2) histotitle = "Count rate difference d(e,e'#pi^{-})X; z ; Q2 (GeV^{2})";
-    if (fA == 12) histotitle = "Count rate difference ^{12}C(e,e'#pi^{-})X; z ; Q2 (GeV^{2})";
 
-    TH2D *countratediff = new TH2D("countratediff",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
-    countratediff->GetXaxis()->CenterTitle(1);
-    countratediff->GetYaxis()->CenterTitle(1);
-    countratediff->Sumw2();
-    countratediff->SetMarkerSize(2);
+    if (fA == 2) histotitle = "Cross section difference e+d (MC error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Cross section difference e+^{12}C (MC error); z ; Q2 (GeV^{2})";
+    TH2D *crosssectiondiff = new TH2D("crosssectiondiff",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    crosssectiondiff->GetXaxis()->CenterTitle(1);
+    crosssectiondiff->GetYaxis()->CenterTitle(1);
+    crosssectiondiff->Sumw2();
+    crosssectiondiff->SetMarkerSize(2);
+
+    if (fA == 2) histotitle = "Count rate for luminosity 1*fb^{-1},  d(e,e'#pi^{+})X (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate for luminosity 1*fb^{-1}, ^{12}C(e,e'#pi^{+})X (statistical error); z ; Q2 (GeV^{2})";
+    TH2D *pionp_counts1 = new TH2D("pionp_counts1 ",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    pionp_counts1->GetXaxis()->CenterTitle(1);
+    pionp_counts1->GetYaxis()->CenterTitle(1);
+    pionp_counts1->SetMarkerSize(2);
+    pionp_counts1->Sumw2();
+    histotitle.Clear();
+
+    if (fA == 2) histotitle = "Count rate for luminosity 0.1*fb^{-1},  d(e,e'#pi^{+})X (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate for luminosity 0.1*fb^{-1}, ^{12}C(e,e'#pi^{+})X (statistical error); z ; Q2 (GeV^{2})";
+    TH2D *pionp_counts2 = new TH2D("pionp_counts2 ",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    pionp_counts2->GetXaxis()->CenterTitle(1);
+    pionp_counts2->GetYaxis()->CenterTitle(1);
+    pionp_counts2->SetMarkerSize(2);
+    pionp_counts2->Sumw2();
+    histotitle.Clear();
+
+    if (fA == 2) histotitle = "Count rate for luminosity 1*fb^{-1},  d(e,e'#pi^{-})X (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate for luminosity 1*fb^{-1}, ^{12}C(e,e'#pi^{-})X (statistical error); z ; Q2 (GeV^{2})";
+    TH2D *pionm_counts1 = new TH2D("pionm_counts1 ",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    pionm_counts1->GetXaxis()->CenterTitle(1);
+    pionm_counts1->GetYaxis()->CenterTitle(1);
+    pionm_counts1->SetMarkerSize(2);
+    pionm_counts1->Sumw2();
+    histotitle.Clear();
+
+    if (fA == 2) histotitle = "Count rate for luminosity 0.1*fb^{-1},  d(e,e'#pi^{-})X (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate for luminosity 0.1*fb^{-1}, ^{12}C(e,e'#pi^{-})X (statistical error); z ; Q2 (GeV^{2})";
+    TH2D *pionm_counts2 = new TH2D("pionm_counts2 ",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    pionm_counts2->GetXaxis()->CenterTitle(1);
+    pionm_counts2->GetYaxis()->CenterTitle(1);
+    pionm_counts2->SetMarkerSize(2);
+    pionm_counts2->Sumw2();
+    histotitle.Clear();
+
+    if (fA == 2)  histotitle = "Count rate difference for luminosity 1*fb^{-1} e+d (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate difference for luminosity 1*fb^{-1} e+^{12}C (statistical) error); z ; Q2 (GeV^{2})";
+    TH2D *countratediff1 = new TH2D("countratediff1",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    countratediff1->GetXaxis()->CenterTitle(1);
+    countratediff1->GetYaxis()->CenterTitle(1);
+    countratediff1->Sumw2();
+    countratediff1->SetMarkerSize(2);
+
+    if (fA == 2)  histotitle = "Statistical relative error count rate difference (luminosity 1*fb^{-1}, e+d); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Statistical relative error count rate difference (luminosity 1*fb^{-1}, e+^{12}C ; z ; Q2 (GeV^{2})";
+    TH2D *relerror_countratediff1 = new TH2D("relerror_countratediff1",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    relerror_countratediff1->GetXaxis()->CenterTitle(1);
+    relerror_countratediff1->GetYaxis()->CenterTitle(1);
+    relerror_countratediff1->Sumw2();
+    relerror_countratediff1->SetMarkerSize(2);
+
+    if (fA == 2)  histotitle = "Count rate difference for luminosity 0.1*fb^{-1} e+d (statistical error); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Count rate difference for luminosity 0.1*fb^{-1} e+^{12}C (statistical) error); z ; Q2 (GeV^{2})";
+    TH2D *countratediff2 = new TH2D("countratediff2",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    countratediff2->GetXaxis()->CenterTitle(1);
+    countratediff2->GetYaxis()->CenterTitle(1);
+    countratediff2->Sumw2();
+    countratediff2->SetMarkerSize(2);
+
+    if (fA == 2)  histotitle = "Statistical relative error count rate difference (luminosity 0.1*fb^{-1}, e+d); z ; Q2 (GeV^{2})";
+    if (fA == 12) histotitle = "Statistical relative error count rate difference (luminosity 0.1*fb^{-1}, e+^{12}C ; z ; Q2 (GeV^{2})";
+    TH2D *relerror_countratediff2 = new TH2D("relerror_countratediff2",histotitle,zbin,z_cut[0],z_cut[zbin],Q2bin,Q2_cut[0],Q2_cut[Q2bin]);
+    relerror_countratediff2->GetXaxis()->CenterTitle(1);
+    relerror_countratediff2->GetYaxis()->CenterTitle(1);
+    relerror_countratediff2->Sumw2();
+    relerror_countratediff2->SetMarkerSize(2);
 
     for(int i=0;i<zbin;i++) {
       for(int j=0;j<Q2bin;j++) {
         //Define
         Double_t Q2, W, Wp, x, y, z, pt, nu, s, gamma, epsilon,rapidity, jacoF;
-        Double_t theta_gen= 0.0 , phi_gen = 0.0, mom_gen = 0.0;
         Double_t mom_gen_ele,mom_gen_had;
         Double_t theta_gen_ele,theta_gen_had;
         Double_t phi_gen_ele,phi_gen_had;
         Double_t theta_q, theta_s,phi_h,phi_s,mom_ele,mom_had,theta_ele, theta_had,phi_ele,phi_had;
         Double_t dxs_incl,dxs_hm,dxs_hp,dilute_hp,dilute_hm;
         Double_t px_ele, py_ele,pz_ele, px_had, py_had, pz_had, E_ele,E_had;
-        Double_t u_pdf, d_pdf, s_pdf, g_pdf, ubar_pdf, dbar_pdf, sbar_pdf;
         Double_t weight_hp, weight_hm, weight_in;
         ULong64_t nsim = 0;
-        //For Beam Position and Vertex info
-        Double_t vx_ele, vy_ele, vz_ele, vx_had, vy_had, vz_had;
-        Double_t D_fav, D_unfav, D_s, D_g;
+
 
         TString filename;
         if(fA ==2){
-          filename= Form("EIC_A2_pion_10_100_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+          if (number_of_files == 500)     filename= Form("fulldataskim/EIC_A2_pion_10_100_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+          else if (number_of_files == 50) filename= Form("50Mdataskim/EIC_A2_pion_10_100_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+          else if (number_of_files == 5) filename = "../../test/massproduction_CTEQ/EIC_A2_pion_10_100_1_total5.root";
+          else     {                    //   filename = "../../test/massproduction_CTEQ/EIC_A2_pion_10_100_1_0.root";
+            //  filename = "EIC_A2_pion_10_100_1_2.root";
+             filename = "../massproduction_CTEQfree/EIC_A2_pion_10_100_1_2.root";
+          }
         }
         if(fA == 12){
-          filename= Form("EIC_A12_pion_10_600_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+          if (number_of_files == 500)     filename= Form("fulldataskim/EIC_A12_pion_10_600_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+	        else if (number_of_files == 50) filename= Form("50Mdataskim/EIC_A12_pion_10_600_skim_Q2bin%d_zbin%d.root",j+1,i+1);
+          else if (number_of_files == 5)  filename = "../../test/massproduction_CTEQ/EIC_A12_pion_10_600_1_total5.root";
+          else                         //   filename = "../../test/massproduction_CTEQ/EIC_A12_pion_10_600_1_0.root";
+           filename = "../massproduction_CTEQfree/EIC_A12_pion_10_600_1_2.root";
         }
 
         TFile *input = new TFile(filename,"R");
@@ -171,25 +280,7 @@ int main(Int_t argc, char *argv[]){
         T->SetBranchAddress("py_had",&py_had);
         T->SetBranchAddress("pz_had",&pz_had);
         T->SetBranchAddress("E_had",&E_had);
-        T->SetBranchAddress("vx_ele",&vx_ele);
-        T->SetBranchAddress("vy_ele",&vy_ele);
-        T->SetBranchAddress("vz_ele",&vz_ele);
-        T->SetBranchAddress("vx_had",&vx_had);
-        T->SetBranchAddress("vy_had",&vy_had);
-        T->SetBranchAddress("vz_had",&vz_had);
 
-        T->SetBranchAddress("u_pdf", &u_pdf);
-        T->SetBranchAddress("d_pdf", &d_pdf);
-        T->SetBranchAddress("s_pdf", &s_pdf);
-        T->SetBranchAddress("g_pdf", &g_pdf);
-        T->SetBranchAddress("ubar_pdf", &ubar_pdf);
-        T->SetBranchAddress("dbar_pdf", &dbar_pdf);
-        T->SetBranchAddress("sbar_pdf", &sbar_pdf);
-
-        T->SetBranchAddress("D_g", &D_g);
-        T->SetBranchAddress("D_s", &D_s);
-        T->SetBranchAddress("D_fav", &D_fav);
-        T->SetBranchAddress("D_unfav", &D_unfav);
 
 
         TString cut = Form("(weight_hp)*(Q2>=%f && Q2<%f && z>=%f && z<%f)",Q2_cut[j],Q2_cut[j+1],z_cut[i],z_cut[i+1]);
@@ -204,44 +295,113 @@ int main(Int_t argc, char *argv[]){
         htemp2->GetXaxis()->CenterTitle(1);
         htemp2->GetXaxis()->SetTitle("Q2");
 
-        TString pipluscut = Form("(weight_hp)*(Q2>=%f && Q2<%f && z>=%f && z<%f)",Q2_cut[j],Q2_cut[j+1],z_cut[i],z_cut[i+1]);
-        TString piminuscut = Form("(weight_hm)*(Q2>=%f && Q2<%f && z>=%f && z<%f)",Q2_cut[j],Q2_cut[j+1],z_cut[i],z_cut[i+1]);
+        TString pipluscut = Form("(1/%i)*(weight_hp)*(Q2>=%f && Q2<%f && z>=%f && z<%f)",number_of_files,Q2_cut[j],Q2_cut[j+1],z_cut[i],z_cut[i+1]);
+        TString piminuscut = Form("(1/%i)*(weight_hm)*(Q2>=%f && Q2<%f && z>=%f && z<%f)",number_of_files,Q2_cut[j],Q2_cut[j+1],z_cut[i],z_cut[i+1]);
         T->Draw("Q2>>htemp",TCut(pipluscut),"");
         T->Draw("Q2>>htemp2",TCut(piminuscut),"");
-      /*  for (int event=0; event<counts_in_bin; event++) {
-            if (weight_hp!=0 && Q2>=Q2_cut[j] && Q2<Q2_cut[j+1] && z>=z_cut[i] && z<z_cut[i+1]) {
+      /* double man_errorintegral_p = 0;
+       double man_errorintegral_m = 0;
+       double man_sumcross_p = 0;
+       double man_sumcross2_p = 0;
+       double man_sumcross_m = 0;
+       double man_sumcross2_m = 0;
+       double test_m = 0;
+       double test2_m = 0;
+       ULong64_t nsim_real = 0;
+
+       for (int event=0; event<counts_in_bin; event++) {
+             T->GetEntry(event);
+	    if (weight_hp!=0 && Q2>=Q2_cut[j] && Q2<Q2_cut[j+1] && z>=z_cut[i] && z<z_cut[i+1]) {
               htemp->Fill(Q2,weight_hp);
+	      nsim_real = dxs_hp*generated_phase_space/weight_hp;
+	      man_sumcross_p+= dxs_hp/pow(nsim_real,1.5);
+	      man_sumcross2_p+=dxs_hp*dxs_hp/pow(nsim_real,2);
+	      test_m += dxs_hp;
+	      test2_m += dxs_hp*dxs_hp;
+	     // cout << "Event " << event << " nsim is " << nsim << " . d*PS/weight+ " << dxs_hp*generated_phase_space/weight_hp << endl;
             }
-            else if (weight_hm!=0 && Q2>=Q2_cut[j] && Q2<Q2_cut[j+1] && z>=z_cut[i] && z<z_cut[i+1]) {
-                htemp2->Fill(Q2,weight_hm);
+            if (weight_hm!=0 && Q2>=Q2_cut[j] && Q2<Q2_cut[j+1] && z>=z_cut[i] && z<z_cut[i+1]) {
+              htemp2->Fill(Q2,weight_hm);
+	      nsim_real = dxs_hm*generated_phase_space/weight_hm;
+	      man_sumcross_m+= dxs_hm/pow(nsim_real,1.5);
+	      man_sumcross2_m+=dxs_hm*dxs_hm/pow(nsim_real,2);
+	     // cout << "Event " << event << " nsim is " << nsim << " . d*PS/weight- " << dxs_hm*generated_phase_space/weight_hm << endl;
             }
-       }*/
+
+	  //  if(!(event%100000)) cout << "Event " << event << " nsim1 is " << nsim_real << " . Test values " << test_m << " and " << test2_m << endl;
+       }
+
+       man_errorintegral_p = generated_phase_space * sqrt(man_sumcross2_p - pow (man_sumcross_p, 2) );
+       man_errorintegral_m = generated_phase_space * sqrt(man_sumcross2_m - pow (man_sumcross_m, 2) );
+        */
        Double_t histsum = (double) htemp->GetSum();
        Double_t histsum2 = (double) htemp2->GetSum();
 
        Double_t histerror = sqrt(htemp->GetSumw2()->GetSum());
        Double_t hist2error = sqrt(htemp2->GetSumw2()->GetSum());
+       cout << "BIN " << i << " and " << j << " ; pi+ sum " << histsum << " , pi+ error " << histerror << " , relative error " << histerror/histsum <<  endl;
+       cout << "BIN " << i << " and " << j << " ; pi- sum " << histsum2 << " , pi- error " << hist2error << " , relative error " << hist2error/histsum2 << endl;
+     //  cout << "BIN " << i << " and " << j << " ; pi+ sum " << histsum << " , pi+ error " << histerror << " , relative error " << histerror/histsum << " , man error " << man_errorintegral_p<< endl;
+     //  cout << "BIN " << i << " and " << j << " ; pi- sum " << histsum2 << " , pi- error " << hist2error << " , relative error " << hist2error/histsum2 << " , man error " << man_errorintegral_m<< endl;
 
        pionp_cross->SetBinContent(i+1,j+1,histsum);
        pionm_cross->SetBinContent(i+1,j+1,histsum2);
 
        pionp_cross->SetBinError(i+1,j+1,histerror);
        pionm_cross->SetBinError(i+1,j+1,hist2error);
+
+
+       Double_t counts1_pion_p = histsum*luminosity*nbarntocm2*integrated_lumi1;
+       Double_t counts2_pion_p = histsum*luminosity*nbarntocm2*integrated_lumi2;
+       Double_t counts1_pion_m = histsum2*luminosity*nbarntocm2*integrated_lumi1;
+       Double_t counts2_pion_m = histsum2*luminosity*nbarntocm2*integrated_lumi2;
+       pionp_counts1->SetBinContent(i+1,j+1,counts1_pion_p);
+       pionp_counts2->SetBinContent(i+1,j+1,counts2_pion_p);
+       pionm_counts1->SetBinContent(i+1,j+1,counts1_pion_m);
+       pionm_counts2->SetBinContent(i+1,j+1,counts2_pion_m);
+
+       pionp_counts1->SetBinError(i+1,j+1,sqrt(counts1_pion_p));
+       pionp_counts2->SetBinError(i+1,j+1,sqrt(counts2_pion_p));
+       pionm_counts1->SetBinError(i+1,j+1,sqrt(counts1_pion_m));
+       pionm_counts2->SetBinError(i+1,j+1,sqrt(counts2_pion_m));
+
+
        delete htemp;
        delete htemp2;
 
       }
     }
 
-    countratediff->Add(pionp_cross,pionm_cross,1,-1);
+    crosssectiondiff->Add(pionp_cross,pionm_cross,1,-1);
+    countratediff1->Add(pionp_counts1,pionm_counts1,1,-1);
+    countratediff2->Add(pionp_counts2,pionm_counts2,1,-1);
+    for(int i=0;i<zbin;i++) {
+      for(int j=0;j<Q2bin;j++) {
+        relerror_countratediff1->SetBinContent(i+1,j+1,countratediff1->GetBinError(i+1,j+1)/countratediff1->GetBinContent(i+1,j+1));
+        relerror_countratediff2->SetBinContent(i+1,j+1,countratediff2->GetBinError(i+1,j+1)/countratediff2->GetBinContent(i+1,j+1));
+
+      }
+    }
+
     TFile *output;
     if (fA == 2) output= new TFile("perbinplots_d.root","RECREATE");
     if (fA == 12) output= new TFile("perbinplots_c12.root","RECREATE");
     output->cd();
-    countratediff->Write();
+
+    histo_counts->Write();
     pionp_cross->Write();
     pionm_cross->Write();
-    histo_counts->Write();
+    crosssectiondiff->Write();
+
+    pionp_counts1->Write();
+    pionp_counts2->Write();
+    pionm_counts1->Write();
+    pionm_counts2->Write();
+    countratediff1->Write();
+    countratediff2->Write();
+
+    relerror_countratediff1->Write();
+    relerror_countratediff2->Write();
 
     output->Close();
 
